55e2095ad9c00041260062567ac221940f6c2e9d
pingulian <jr.schoenborn@icloud.com>
2025-01-17 23:08:16 +0100

    Update Temperatur_Arduino_Julian.ino

--- Temperatur_Arduino_Julian.ino
+++ Temperatur_Arduino_Julian.ino
@@ -6,36 +6,21 @@ BLEService serialService("180C"); // Benutzerdefinierter Service
 BLECharacteristic serialCharacteristic("2A56", BLERead | BLENotify, 512);
 
 void setup() {
-  // Status-LED zum Testen hinzufügen
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH); // LED einschalten, um zu zeigen, dass der Arduino läuft
-  
-  // Serielle Kommunikation starten (falls nötig)
+
   Serial.begin(9600);
-  while (!Serial); // Warten, bis der serielle Monitor bereit ist
-  
-  // Kleine Verzögerung, um sicherzustellen, dass das BLE-Modul genügend Zeit zum Starten hat
-  delay(2000);  // Warte 2 Sekunden
+  delay(2000); 
 
   // BLE-Initialisierung
   if (!BLE.begin()) {
     Serial.println("BLE konnte nicht gestartet werden");
     while (1); // Endlosschleife bei Fehler
   }
-
-  // BLE-Konfiguration
   BLE.setLocalName("Nano33BLE");         // Anzeigename im BLE-Scanner
-
-  // Charakteristik dem Service hinzufügen
   serialService.addCharacteristic(serialCharacteristic);
-
-  // Service zum BLE hinzufügen
   BLE.addService(serialService);
-
-  // Charakteristik initialisieren
   serialCharacteristic.writeValue("Initial Value");
-
-  // Service der Werbung hinzufügen und Werbung starten
   BLE.setAdvertisedService(serialService);
   if (!BLE.advertise()) {
     Serial.println("BLE-Werbung konnte nicht gestartet werden");
@@ -74,5 +59,5 @@ void loop() {
     Serial.println("Verbindung getrennt");
   }
 
-  delay(500); // Vermeidung übermäßiger CPU-Auslastung
+  delay(500); // Vermeidung übermäßiger Belastung
 }
